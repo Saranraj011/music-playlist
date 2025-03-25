210889b9c73260b90c0ca37cf98f70f412e0cea9
SARAN <vjsaran346@gmail.com>
2025-03-25 23:44:16 +0530

    Joke Generator

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 
 function App(){
-  const [color,setColor]=useState("");
+  const [joke,setJoke]=useState("");
+  const [loading,setLoading]=useState(false);
+
+  const fetchJoke = async () => {
+   setLoading(true);
+
+   try {
+    const response = await fetch("");
+    const data = await response.json();
+    setJoke(`${data.setup} - ${data.punchline}`);
+   } catch (error) {
+    setJoke("Oops! Failed to load joke, try again");
+   }
+   setLoading(false);
+  };
+
+  useEffect(()=> {
+    fetchJoke();
+  },[]);
 
   return (
-    <div style={{...contStyle,backgroundColor:color ||"white"}}>
-
-      <h2>Dynamic color changer</h2>
-      
-         <input
-        type="text"
-        placeholder="Enter color e.g red,blue" 
-        value={color}
-        onChange={(e)=>setColor(e.target.value)}
-        style={inputStyle}
-        />
-
-        <p>Try colors like  <b>blue,red</b>or HEX value like<b>#ff5733</b></p>
-
+    <div style={contStyle}>
+      <h2>Random Joke generator using API</h2>
+      {loading ?<p>Loading...</p>:<p style={jokeStyle}>{joke}</p>}
+       
+       <button onClick={fetchJoke} style={btnStyle}>Get New Joke</button>
     </div>
   );
 }
@@ -31,36 +40,36 @@ const contStyle={
   transition:"backgroung-color 0.5s ease"
 };
 
-// const btnStyle={
-//   margin:"10px",
-//   padding:"10px 20px",
-//   fontSize:"18px",
-//   cursor:"pointer"
-// };
-// const formStyle={
-//   display:"flex",
-//   flexDirection: "column", 
-//   alignItems: "center", 
-//   gap: "10px" 
-// };
+const btnStyle={
+  margin:"10px",
+  padding:"10px 20px",
+  fontSize:"18px",
+  cursor:"pointer"
+};
+const formStyle={
+  display:"flex",
+  flexDirection: "column", 
+  alignItems: "center", 
+  gap: "10px" 
+};
 
-const inputStyle = { 
-  padding: "10px", 
-  fontSize: "16px",
-   width: "300px",
-   margin: "10px",
-   border: "1px solid #ccc",
-   borderRadius: "5px",
-   textAlign:"center" 
-  }; 
-
-// const outputStyle = { 
-//   marginTop: "20px", 
+// const inputStyle = { 
 //   padding: "10px", 
-//   border: "1px solid #ccc", 
-//   borderRadius: "5px", 
-//   display: "inline-block" 
-// };
+//   fontSize: "16px",
+//    width: "300px",
+//    margin: "10px",
+//    border: "1px solid #ccc",
+//    borderRadius: "5px",
+//    textAlign:"center" 
+//   }; 
+
+const jokeStyle = { 
+  marginTop: "20px", 
+  padding: "10px", 
+  border: "1px solid #ccc", 
+  borderRadius: "5px", 
+  display: "inline-block" 
+};
 
 
 export default App;
